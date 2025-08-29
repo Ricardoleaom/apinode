277a82f7afd738ed0dd5bff496d544104c0e8491
Ricardoleaom <ricardoleaom@gmail.com>
2025-08-29 18:30:18 -0300

    feat: atualizacao geral

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,49 +1,67 @@
-// MariaCecilia98@bol.com.br
-
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses, users } from "../database/schema.ts";
+import { sign } from "jsonwebtoken";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
-import { verify } from "argon2"
+import { verify } from "argon2";
 
 export const loginRoute: FastifyPluginAsyncZod = async (server) => {
-server.post("/sessions", {
-  schema: {
-    tags: ["auth"],
-    summary: "Login",
-    description: "Essa rota recebe um email e uma senha e cria uma sessão no banco de dados.",
-    body: z.object({
-      email: z.email(),
-      password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
-    }),
-    // response: {
-    //   201: z.object({
-    //     sessionId: z.string().uuid(),
-    //   }),
-    // },
-  },
-}, async (request, reply) => {
-  const { email, password } = request.body;
+  server.post(
+    "/sessions",
+    {
+      schema: {
+        tags: ["auth"],
+        summary: "Login",
+        description:
+          "Essa rota recebe um email e uma senha e cria uma sessão no banco de dados.",
+        body: z.object({
+          email: z.email(),
+          password: z
+            .string()
+            .min(6, "A senha deve ter pelo menos 6 caracteres"),
+        }),
+        response: {
+          200: z.object({ token: z.string() }),
+          400: z.object({ message: z.string() }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body;
 
-  // Aqui você deve implementar a lógica de autenticação, como verificar o email e a senha no banco de dados.
+      // Aqui você deve implementar a lógica de autenticação, como verificar o email e a senha no banco de dados.
 
-  const result = await db.select()
-  .from(users)
-  .where(eq(users.email, email))
+      const result = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email));
 
-  if (result.length === 0) {
-    return reply.status(400).send({ message: "Credencias Invalidas." });
-  }
+      if (result.length === 0) {
+        return reply.status(400).send({ message: "Credencias Invalidas." });
+      }
 
-  const user = result[0]; // Substitua por uma lógica real de geração de ID de sessão.
+      const user = result[0]; // Substitua por uma lógica real de geração de ID de sessão.
 
-  const doesPasswordMatch = await verify(user.password, password)
+      const doesPasswordMatch = await verify(user.password, password);
 
-  if (!doesPasswordMatch) {
-    return reply.status(400).send({ message: "Credenciais Inválidas." });
-  }
+      if (!doesPasswordMatch) {
+        return reply.status(400).send({ message: "Credenciais Inválidas." });
+      }
 
-  return reply.status(200).send({ message: "OK" });
-});
-}
+      if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not set");
+      }
+
+      const token = sign(
+        { sub: user.id, role: user.role },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      return reply.status(200).send({ token });
+    }
+  );
+};
